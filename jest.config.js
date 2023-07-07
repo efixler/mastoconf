eb9f51d1a36569bd8a43ef072a76627e164d7187
Eric Fixler <efixler@mozilla.com>
2023-07-06 22:16:38 -0400

    module and build updates

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/*.spec.ts?(x)'],
+    testMatch: ['/**/*.spec.ts?(x)'],
     testPathIgnorePatterns: ['/dist/'],
   };
